4fe93aa11cec5bcf11b41a823d3ed51987a04004
saber0amine <saberamine000@gmail.com>
2024-07-15 01:40:07 +0100

    add the solution

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,34 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-	  if (err) throw err;
-	  const actors = JSON.parse(body).characters;
-	  exactOrder(actors, 0);
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Invalid status code:', response.statusCode);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  const characterUrls = movie.characters;
+
+  const fetchCharacter = (url) => {
+    return new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) reject(error);
+        else if (response.statusCode !== 200) reject(new Error(`Invalid status code: ${response.statusCode}`));
+        else resolve(JSON.parse(body).name);
+      });
+    });
+  };
+
+  Promise.all(characterUrls.map(fetchCharacter))
+    .then(names => names.forEach(name => console.log(name)))
+    .catch(error => console.error('Error:', error));
 });
-const exactOrder = (actors, x) => {
-	  if (x === actors.length) return;
-	  request(actors[x], function (err, res, body) {
-		      if (err) throw err;
-		      console.log(JSON.parse(body).name);
-		      exactOrder(actors, x + 1);
-		    });
-};
